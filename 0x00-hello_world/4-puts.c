0b9bde78012a5a4a34adc09f06c3f9bee8aaf081
Opara Chinedu <oparachinedu90@gmail.com>
2022-06-30 20:31:24 -0700

    Fixed whitespace error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 /**
- *  * main -	'"Programming is like building a multilingual puzzle'
- *Return:Always0(Success)  
-    */
+* main - Entry point
+*Return:Always0(Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
