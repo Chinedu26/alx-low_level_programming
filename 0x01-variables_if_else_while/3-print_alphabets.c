604d5b78ddecaada748e526148092b83f3f6906b
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-02 03:28:00 -0700

    Modified alphABET file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,14 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a', ch <= 'z', ch++)
+	for (ch = 97; ch <= 122, ch++)
+	{
 		putchar(ch);
-	for (ch = 'A', ch <= 'Z', ch++)
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
 		putchar(ch);
-	putchar('\n');
+	}
+	putchar(10); /* this is the ASCII code for new line*/
 	return (0);
 }
