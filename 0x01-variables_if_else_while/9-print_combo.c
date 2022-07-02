19c357ed11de3b3b4b3a725f1f8170a82a80c148
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-02 05:16:53 -0700

    Fingers crossed task 9

--- 0x01-variables_if_else_while/9-print_combo.c
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -5,19 +5,17 @@
  */
 int main(void)
 {
-	int ch;
+	int n;
 
-	for (ch = 48; ch <= 57; ch++)
+	for (n = 48; n < 58; n++)
 	{
-	putchar(ch);
-	if (ch != 57)
+	putchar(n);
+	if (n != 57)
 	{
-	putchar (44);
-	putchar (32);
+		putchar(',');
+		putchar('');
 	}
 	}
-
-	putchar (10); /*this is an ascii code for new line*/
-
+	putchar('\n');
 	return (0);
 }
