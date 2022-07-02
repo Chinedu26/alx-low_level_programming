3a6f91a89b0b76572ee0f750e310c312d24c0f69
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-02 05:40:08 -0700

    perseverance

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdio.h>
 /**
  * main - main block
  * Return: 0
@@ -12,7 +10,7 @@ int main(void)
 	for (ch = 48; ch <= 57; ch++)
 	{
 	putchar(ch);
-	if ch != 57)
+	if (ch != 57)
 	{
 	putchar(44);
 	putchar(32);
