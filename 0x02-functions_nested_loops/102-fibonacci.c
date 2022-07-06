d23b81fcef92698db32915461c7b8891ec40da29
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-06 16:20:13 -0700

    Steal password of students

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "main.h"
+/**
+ * main - print the first 50 fibonacci numbers
+ * Return: Nothing
+ */
+int main(void)
+{
+	int count;
+	unsigned long a, b, c;
+
+	a = 0;
+	b = 1;
+	for (count = 1; count <= 49; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu, ", c);
+	}
+	printf("%lu\n", c + a);
+	return (0);
+}
