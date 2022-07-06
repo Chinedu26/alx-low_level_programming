43a625cff18ef276afae689fa5de6ad2a359ca3f
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-06 14:21:43 -0700

    Modified 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - check if c is a letter, upper or lowercase
- * @c - character to be checked if alpha
+ * @c: character to be checked if alpha
  * Return: 0 or 1
  */
 int _isalpha(int c)
