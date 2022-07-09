8a686e467f988a8d6a4f6fef07ee52c6118125f9
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-08 21:09:32 -0700

    Fingers crossed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	int i;
+	/*int i;*/
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+	/*i = 0;*/
 	/*
 	 * while (i < 10)
 	 * {
@@ -19,4 +19,4 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
-i}
+}
