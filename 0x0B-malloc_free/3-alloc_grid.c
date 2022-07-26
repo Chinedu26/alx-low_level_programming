9ecaf6524ae3cec3bf7d74cacd91f6e4b45df7c9
Opara Chinedu <oparachinedu90@gmail.com>
2022-07-25 18:35:39 -0700

    modified

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,25 +7,29 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **matrix, row, column;
+	int **twoD;
+	int hgt_index, wid_index;
 
-	if (width < 1 || height < 1)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	matrix = (int **) malloc(sizeof(int *) * height);
-	if (!matrix)
+	twoD = malloc(sizeof(int *) * height);
+	if (twoD == NULL)
 		return (NULL);
-	for (row = 0; row < height; ++row)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		matrix[row] = (int *) malloc(sizeof(int) * width);
-		if (!matrix[row])
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+		if (twoD[hgt_index] == NULL)
 		{
-			while (--row > -1)
-				free(matrix[row]);
-			free(matrix);
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+			free(twoD);
 			return (NULL);
 		}
-		for (column = 0; column < width; ++column)
-			matrix[row][column] = 0;
 	}
-	return (matrix);
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
+	return (twoD);
 }
