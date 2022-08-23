e3b72b04d516c62e1f37d49cb515c4ee85c0b7ba
Chinedu26 <106815468+Chinedu26@users.noreply.github.com>
2022-08-23 22:20:25 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,36 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints the letters.
+ * read_textfile - reads a text file and prints the letters
  * @filename: filename.
- * @letters: number of letters printed.
- * Return: number of lettreers printed. It fails, return 0.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t lenr, lenw;
-	char *buffer;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	fd = open(filename, 0_RDONLY);
+
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
-	lenr = read(fd, buffer, letters);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	if (lenr == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
-		return (0);
-	return (lenw);
+
+	free(buf);
+
+	return (nwr);
 }
